40a0563aaefd6a856f1f7de5b477fd71a015de17
yoga220802 <yogaagustiansyah01@gmail.com>
2024-12-17 01:45:06 +0700

    feat: add root endpoint for testing

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
 
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/', authRoutes);
+app.use('/auth', authRoutes);
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: 'API berhasil dibuat cuyy' });
+  });
 
 export default app;
