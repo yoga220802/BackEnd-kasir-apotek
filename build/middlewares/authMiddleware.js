65d8934bc2876ab7b6ef3a73b625212b1b6f0fdd
yoga220802 <yogaagustiansyah01@gmail.com>
2024-12-17 02:11:24 +0700

    update tsconfig

--- build/middlewares/authMiddleware.js
+++ build/middlewares/authMiddleware.js
@@ -6,8 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const verifyToken = (req, res, next) => {
-    var _a;
-    const token = (_a = req.header('Authorization')) === null || _a === void 0 ? void 0 : _a.split(' ')[1]; // Format: Bearer TOKEN
+    const token = req.header('Authorization')?.split(' ')[1]; // Format: Bearer TOKEN
     if (!token) {
         return res.status(401).json({ message: 'Akses ditolak, token tidak ada' });
     }
--- build/server.js
+++ build/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,12 +7,12 @@ const database_1 = require("./config/database");
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT;
 // Sinkronisasi database dan jalankan server
-(() => __awaiter(void 0, void 0, void 0, function* () {
+(async () => {
     try {
-        yield database_1.sequelize.authenticate();
+        await database_1.sequelize.authenticate();
         console.log('Koneksi ke database berhasil.');
         // Sinkronisasi model
-        yield database_1.sequelize.sync({ force: false }); // Gunakan `force: true` jika ingin mereset tabel setiap kali server dijalankan
+        await database_1.sequelize.sync({ force: false }); // Gunakan `force: true` jika ingin mereset tabel setiap kali server dijalankan
         console.log('Model disinkronkan.');
         app_1.default.listen(PORT, () => {
             console.log(`Server berjalan di http://localhost:${PORT}`);
@@ -30,4 +21,4 @@ const PORT = process.env.PORT;
     catch (err) {
         console.error('Gagal terhubung ke database:', err);
     }
-}))();
+})();
